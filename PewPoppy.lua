a60873e83dd839c376618df99a56029d1b0a227f
PewPewPew2 <tschweitzer@live.com>
2016-05-05 09:49:22 -0600

    Update PewPoppy.lua

--- PewPoppy.lua
+++ PewPoppy.lua
@@ -1,4 +1,4 @@
-if myHero.charName~='Poppy' then return end
+-- if myHero.charName~='Poppy' then return end
 
 local pi, pi2, atan, cos, sin, sqrt = math.pi, math.pi*2, math.atan, math.cos, math.sin, math.sqrt
 
@@ -54,10 +54,33 @@ local function GeneratePoints(range, pMax, ePos, sPos)
 	end
 	return points
 end
-
-
-print('PewPoppy')
 require('HPrediction')
+
+AddLoadCallback(function()
+	local version = 0.02
+	ScriptUpdate(
+		version,
+		true, 
+		'raw.githubusercontent.com', 
+		'/PewPewPew2/BoL/master/Versions/PewPoppy.version', 
+		'/PewPewPew2/BoL/master/PewPoppy.lua', 
+		SCRIPT_PATH.._ENV.FILE_NAME, 
+		function() Print('Update Complete. Please reload. (F9 F9)') end, 
+		function() Print('Loaded latest version. v'..version..'.') end, 
+		function() Print('New version found, downloading now...') end,
+		function() Print('There was an error during update.') end
+	)
+
+end)
+
+function Print(text, isError)
+	if isError then
+		print('<font color=\'#0099FF\'>[PewPoppy] </font> <font color=\'#FF0000\'>'..text..'</font>')	
+		return
+	end
+	print('<font color=\'#0099FF\'>[PewPoppy] </font> <font color=\'#FF6600\'>'..text..'</font>')
+end
+
 local Enemies = {}
 for i=1, heroManager.iCount do
 	local h = heroManager:getHero(i)
@@ -201,7 +224,7 @@ AddNewPathCallback(function(unit,startPos,endPos,isDash,dashSpeed,dashGravity,da
 	if unit.valid and isDash and unit.type == 'AIHeroClient' then
 		if unit.isMe then
 			amDashing = os.clock() + (GetDistance(startPos, endPos) / dashSpeed) - (GetLatency() * 0.0005)
-		elseif unit.team ~= myHero.team and amDashing < os.clock() then
+		elseif unit.team ~= myHero.team and amDashing < os.clock() and GetDistanceSqr(startPos, endPos) > 22500 then
 			local dp, bp = GetLinePoint(startPos.x, startPos.z, endPos.x, endPos.z, myHero.x, myHero.z)
 			if (bp and GetDistanceSqr(dp) < 160000) or GetDistanceSqr(endPos) < 160000 or GetDistanceSqr(startPos) < 160000 then
 				CastSpell(_W)
@@ -294,7 +317,7 @@ AddTickCallback(function()
 			end
 		end
 		if myHero:CanUseSpell(_E) == READY then
-			local t = _Pewalk.GetTarget(700)
+			local t = _Pewalk.GetTarget(425 + myHero.boundingRadius, true)
 			if t then
 				if checkE(t) or pMenu.ForceE then
 					if immuneCheck(t) then
@@ -303,7 +326,7 @@ AddTickCallback(function()
 				end
 			end
 			for i, e in ipairs(Enemies) do
-				if e~=t and _Pewalk.ValidTarget(e, 700) and immuneCheck(e) then
+				if e~=t and _Pewalk.ValidTarget(e, 425 + myHero.boundingRadius, true) and immuneCheck(e) then
 					if _Pewalk.IsHighPriority(e, 2) and checkE(e) then
 						CastSpell(_E, e)
 					end
@@ -330,12 +353,13 @@ AddTickCallback(function()
 	end	
 	if pMenu.FlashKey and pFlash then
 		if myHero:CanUseSpell(_E) == READY and myHero:CanUseSpell(pFlash) == READY then
-			local t = _Pewalk.GetTarget(1000)
+			local t = _Pewalk.GetTarget(850)
+			local d = (425 + myHero.boundingRadius) ^ 2
 			if t and immuneCheck(t) then
 				local myPos = NormalizeX(GetPath(myHero), myHero, (GetLatency()*0.0005)*myHero.ms)
 				local tPos = NormalizeX(GetPath(t), t, (GetLatency()*0.0005)*t.ms)
 				for _, p in ipairs(GeneratePoints(400, 7, tPos, myPos)) do
-					if GetDistanceSqr(p, tPos) < 390625 and not IsWall(D3DXVECTOR3(p.x,myHero.y,p.z)) and checkE(tPos, p) then
+					if GetDistanceSqr(p, tPos) < d and not IsWall(D3DXVECTOR3(p.x,myHero.y,p.z)) and checkE(tPos, p) then
 						CastSpell(pFlash, p.x, p.z)
 						DelayAction(function() CastSpell(_E, t) end)
 						return
@@ -391,3 +415,194 @@ AddAnimationCallback(function(unit, animation)
 		end
 	end
 end)
+
+class "ScriptUpdate"
+function ScriptUpdate:__init(LocalVersion,UseHttps, Host, VersionPath, ScriptPath, SavePath, CallbackUpdate, CallbackNoUpdate, CallbackNewVersion,CallbackError)
+    self.LocalVersion = LocalVersion
+    self.Host = Host
+    self.VersionPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..VersionPath)..'&rand='..math.random(99999999)
+    self.ScriptPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..ScriptPath)..'&rand='..math.random(99999999)
+    self.SavePath = SavePath
+    self.CallbackUpdate = CallbackUpdate
+    self.CallbackNoUpdate = CallbackNoUpdate
+    self.CallbackNewVersion = CallbackNewVersion
+    self.CallbackError = CallbackError
+    AddDrawCallback(function() self:OnDraw() end)
+    self:CreateSocket(self.VersionPath)
+    self.DownloadStatus = 'Connect to Server for VersionInfo'
+    AddTickCallback(function() self:GetOnlineVersion() end)
+end
+
+function ScriptUpdate:print(str)
+    print('<font color="#FFFFFF">'..os.clock()..': '..str)
+end
+
+function ScriptUpdate:OnDraw()
+    if self.DownloadStatus ~= 'Downloading Script (100%)' and self.DownloadStatus ~= 'Downloading VersionInfo (100%)'then
+        DrawText('Download Status: '..(self.DownloadStatus or 'Unknown'),50,10,50,ARGB(0xFF,0xFF,0xFF,0xFF))
+    end
+end
+
+function ScriptUpdate:CreateSocket(url)
+    if not self.LuaSocket then
+        self.LuaSocket = require("socket")
+    else
+        self.Socket:close()
+        self.Socket = nil
+        self.Size = nil
+        self.RecvStarted = false
+    end
+    self.LuaSocket = require("socket")
+    self.Socket = self.LuaSocket.tcp()
+    self.Socket:settimeout(0, 'b')
+    self.Socket:settimeout(99999999, 't')
+    self.Socket:connect('sx-bol.eu', 80)
+    self.Url = url
+    self.Started = false
+    self.LastPrint = ""
+    self.File = ""
+end
+
+function ScriptUpdate:Base64Encode(data)
+    local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    return ((data:gsub('.', function(x)
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+function ScriptUpdate:GetOnlineVersion()
+    if self.GotScriptVersion then return end
+
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: sx-bol.eu\r\n\r\n")
+    end
+    if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
+        self.RecvStarted = true
+        self.DownloadStatus = 'Downloading VersionInfo (0%)'
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</s'..'ize>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading VersionInfo ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') then
+        self.DownloadStatus = 'Downloading VersionInfo (100%)'
+        local a,b = self.File:find('\r\n\r\n')
+        self.File = self.File:sub(a,-1)
+        self.NewFile = ''
+        for line,content in ipairs(self.File:split('\n')) do
+            if content:len() > 5 then
+                self.NewFile = self.NewFile .. content
+            end
+        end
+        local HeaderEnd, ContentStart = self.File:find('<scr'..'ipt>')
+        local ContentEnd, _ = self.File:find('</sc'..'ript>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            self.OnlineVersion = (Base64Decode(self.File:sub(ContentStart + 1,ContentEnd-1)))
+            self.OnlineVersion = tonumber(self.OnlineVersion)
+            if self.OnlineVersion and self.OnlineVersion > self.LocalVersion then
+                if self.CallbackNewVersion and type(self.CallbackNewVersion) == 'function' then
+                    self.CallbackNewVersion(self.OnlineVersion,self.LocalVersion)
+                end
+                self:CreateSocket(self.ScriptPath)
+                self.DownloadStatus = 'Connect to Server for ScriptDownload'
+                AddTickCallback(function() self:DownloadUpdate() end)
+            else
+                if self.CallbackNoUpdate and type(self.CallbackNoUpdate) == 'function' then
+                    self.CallbackNoUpdate(self.LocalVersion)
+                end
+            end
+        end
+        self.GotScriptVersion = true
+    end
+end
+
+function ScriptUpdate:DownloadUpdate()
+    if self.GotScriptUpdate then return end
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: sx-bol.eu\r\n\r\n")
+    end
+    if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
+        self.RecvStarted = true
+        self.DownloadStatus = 'Downloading Script (0%)'
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</si'..'ze>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading Script ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') then
+        self.DownloadStatus = 'Downloading Script (100%)'
+        local a,b = self.File:find('\r\n\r\n')
+        self.File = self.File:sub(a,-1)
+        self.NewFile = ''
+        for line,content in ipairs(self.File:split('\n')) do
+            if content:len() > 5 then
+                self.NewFile = self.NewFile .. content
+            end
+        end
+        local HeaderEnd, ContentStart = self.NewFile:find('<sc'..'ript>')
+        local ContentEnd, _ = self.NewFile:find('</scr'..'ipt>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            local newf = self.NewFile:sub(ContentStart+1,ContentEnd-1)
+            local newf = newf:gsub('\r','')
+            if newf:len() ~= self.Size then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+                return
+            end
+            local newf = Base64Decode(newf)
+            if type(load(newf)) ~= 'function' then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+            else
+                local f = io.open(self.SavePath,"w+b")
+                f:write(newf)
+                f:close()
+                if self.CallbackUpdate and type(self.CallbackUpdate) == 'function' then
+                    self.CallbackUpdate(self.OnlineVersion,self.LocalVersion)
+                end
+            end
+        end
+        self.GotScriptUpdate = true
+    end
+end
